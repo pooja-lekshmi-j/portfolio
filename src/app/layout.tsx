bf292edd93b612b45b3001d3d2232a609253fb6d
pooja-lekshmi-j <pujalaxmi345@gmail.com>
2026-05-02 16:52:53 +0530

    integrate Vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -37,8 +38,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en" className={inter.variable}>
+        <body className={inter.className}>{children}
+          <SpeedInsights />
+        </body>
+      </html>
   );
 }
