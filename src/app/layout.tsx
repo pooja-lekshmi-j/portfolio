6cb666a9b168868c808439ec01d331db0e7677dc
pooja-lekshmi-j <pujalaxmi345@gmail.com>
2026-05-02 17:02:52 +0530

    add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({
@@ -41,6 +42,7 @@ export default function RootLayout({
       <html lang="en" className={inter.variable}>
         <body className={inter.className}>{children}
           <SpeedInsights />
+          <Analytics />
         </body>
       </html>
   );
